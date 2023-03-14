669852c07a5def08dae73921bad9e489d8811327
aleveil <anthony.leveil@protonmail.com>
2023-03-14 12:13:00 +0100

    add authentification, wrong email/psswrd error and success login page

--- script.js
+++ script.js
@@ -2,12 +2,25 @@ const button = document.querySelector("#button");
 const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
 const errorMessage = document.querySelector("#error-msg");
+const form = document.querySelector("form");
+const success = document.querySelector("#success-login");
 
 button.addEventListener("click", () => {
-	console.log(emailInput.value);
 	if (emailInput.value === "" || passwordInput.value === "") {
+		errorMessage.innerHTML = "All fields must be filled !";
 		errorMessage.classList.remove("hidden");
 	}
 	else
-		errorMessage.classList.add("hidden");
+	{
+		if (emailInput.value === "john.doe@example.com" && passwordInput.value === "Test12345") {
+			errorMessage.classList.add("hidden");
+			form.classList.add("hidden");
+			success.classList.remove("hidden");
+		}
+		else {
+			errorMessage.innerHTML = "Wrong email and/or password !";
+			errorMessage.classList.remove("hidden");
+		}
+	}
+	
 });
